817d85174308526fbb27ff5e11972feded875640
TrueBallin <ballin_true@yahoo.com>
2020-02-09 00:44:01 -0800

    Updated Rainbow-app

--- Rainbowx3/Rainbowx3/ViewController/ColorsDetailViewController.swift
+++ Rainbowx3/Rainbowx3/ViewController/ColorsDetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ColorsDetailViewController: UIViewController {
 
+    
+    var cellColor: 
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Rainbowx3/Rainbowx3/ViewController/ColorsTableViewController.swift
+++ Rainbowx3/Rainbowx3/ViewController/ColorsTableViewController.swift
@@ -9,28 +9,32 @@
 import UIKit
 
 class ColorsTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
+    var colors: [MyColor] = [
+        MyColor(name: "Red", color: .red),
+        MyColor(name: "Orange", color: .orange),
+        MyColor(name: "Black", color: .black),
+        MyColor(name: "Green", color: .green),
+        MyColor(name: "Blue", color: .blue),
+        MyColor(name: "Yellow", color: .yellow),
+        MyColor(name: "Violet"  , color: .purple),
+        MyColor(name: "Indego", color: .cyan)
+    ]
     // MARK: - Table view data source
 
    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return colors.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        
+        let color = colors[indexPath.row]
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.color
+        
         return cell
     }
     
